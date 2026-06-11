1a9fb8bba21dd250e066e711b95cb938647d7f02
joilg <joilg@users.noreply.github.com>
2026-06-11 19:08:02 +0200

    correct ea6521 Chip pins.   fix provisioning script
    the right SKW chip select pins are:
    gpio_host_wake =  <0x24 0x12 0x00>
    gpio_chip_wake = <0x24 0x11 0x00>
    gpio_chip_en = <0x24 0x06 0x00>
    fix provisioninf script
    Thanks to  <8lall0> for contributing.

--- userpatches/overlay/root/provisioning.sh
+++ userpatches/overlay/root/provisioning.sh
@@ -26,27 +26,14 @@ systemctl start vfd-network.service
 }
 #####################################################
 ea6621q() {
-if  [ -d "/sys/kernel/debug/mmc2/mmc2:8800" ]; then  
-echo "Seekwave Wifi6 Chip detected"
-echo "Install Seekwave EA6521 driver"
-
-#  Chip Name     Driver     DKMS-Version   wifi chip id :
-#	LGX8800G		aic8800-sdio     5.0          ac2a    
-#	EA6521QF   		ea6621q            1.0          8800   
 if  dkms status ea6621q | grep installed; then
-dkms remove  -m ea6621q -v 1.0
+dkms remove  -m ea6621q -v 1.0 >>/var/log/provisioning
 fi
 
 # dkms remove -m ea6621q -v 1.0  | true
 dkms add -m ea6621q -v 1.0 >>/var/log/provisioning
 dkms build -m ea6621q -v 1.0 >>/var/log/provisioning
 dkms install -m ea6621q -v 1.0 >>/var/log/provisioning
-
-modprobe skw_sdio
-modprobe skw_bootcoms
-modprobe skw
-modprobe skwbt
-
 
 cat <<EOF > /etc/modules-load.d/skw.conf
 hidp
@@ -59,8 +46,6 @@ skwbt
 EOF
 
 echo "Seekwave EA6521 driver installed. Use armbian-config to connect via Wi-Fi" 
-
-fi
 }
 ####################################################
 firmware() {
@@ -72,15 +57,11 @@ cp dkms-ea6x21q/ea6x21p/firmware/*  /usr/lib/firmware/skw/
 
 ###################################################
 blacklist() {
-if  [ -d "/sys/kernel/debug/mmc2/mmc2:8800" ]; then  
-echo
 echo "Wifi Chip = ea6521    blacklist AIC8800 Driver "
 mv /etc/modules-load.d/aic8800-btlpm.conf /etc/modules-load.d/aic8800-btlpm.bak 2>>/var/log/provisioning 
-echo "blacklist aic8800_fdrv" > /etc/modprobe.d/blacklist-aic8800.conf 
+echo "blacklist aic8800_fdrv" >> /etc/modprobe.d/blacklist-aic8800.conf 
 echo "blacklist aic8800_btlpm" >> /etc/modprobe.d/blacklist-aic8800.conf 
 echo "blacklist aic8800_bsp" >> /etc/modprobe.d/blacklist-aic8800.conf 
-echo 
-fi
 }
 
 
@@ -89,13 +70,32 @@ echo "Provisioning started"
 echo " Provisioning $(date '+%B %V %T.%3N:')   "    >/var/log/provisioning
 echo
 openvfd
-blacklist
-# firmware  # already installed
-ea6621q
 
+DEVICE_ID=$(cat /sys/bus/sdio/devices/*/device 2>/dev/null | head -n 1)
+#  Chip Name     Driver     DKMS-Version   wifi chip id :
+#	LGX8800G		aic8800-sdio     5.0          ac2a    
+#	EA6521QF   		ea6621q            1.0          8800   
+
+case "$DEVICE_ID" in
+    "0x0145"|"0x0146")
+        #AIC8800 WIFI
+	   echo "AIC8800 Wifi6 Chip detected"
+		;;
+    "0x0000")
+	   # Seekwave EA6521
+	   echo "Seekwave Wifi6 Chip detected"
+		echo "Install Seekwave EA6521 driver"
+   		blacklist
+		ea6621q        
+        ;;
+    *)
+        echo "Unbekannter SDIO Chip oder kein Gerät gefunden: $DEVICE_ID" >> /var/log/wifi-init.log
+        ;;
+esac
+echo
+echo "Provisioning complete. log info in /var/log/provisioning"
 echo "Please reboot to apply the changes."
 echo 
-echo "Provisioning complete. log info in /var/log/provisioning"
 
 
 
